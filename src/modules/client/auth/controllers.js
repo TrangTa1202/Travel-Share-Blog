f5e3b7d02eb3ef5b3b9745db75c0a89035e3b26c
Duy Quang <quangnd@wiicamp.com>
2023-10-11 21:40:12 +0700

    [Travel Share Blog] [Authentication] API Signup [N1TSB-11] Update feature sign up

--- src/modules/client/auth/controllers.js
+++ src/modules/client/auth/controllers.js
@@ -1,17 +1,25 @@
-const { User } =require('../../../models')
+const { User } = require('../../../models');
 
 module.exports = {
-  register: async (req, res) => {
-    const { firstName, lastName, username, email, password } = req.body
+  register: async (req, res, next) => {
+    try {
+      const {
+        firstName, lastName, username, email, password,
+      } = req.body;
 
-    const user = await User.findOne({ email })
+      const user = await User.findOne({ email });
 
-    if (user) {
-      return res.status(400).json({ message: 'Email already exists' })
+      if (user) {
+        return res.status(400).json({ message: 'Email already exists' });
+      }
+
+      await User.create({
+        firstName, lastName, username, email, password,
+      });
+
+      return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+      next(error);
     }
-
-    await User.create({ firstName, lastName, username ,email, password })
-
-    return res.status(201).json({ message: 'User created successfully' })
   },
 };
