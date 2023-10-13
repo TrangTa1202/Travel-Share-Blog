d5abb7a68e1640cd3970c4e7c2b3f8c1418190b7
Duy Quang <quangnd@wiicamp.com>
2023-10-13 21:39:16 +0700

    [Travel Share Blog] [Authentication] API login [N1TSB-12] Update feature login [IssueType] Story [Test] [Pass] Ngọc

--- src/modules/client/auth/controllers.js
+++ src/modules/client/auth/controllers.js
@@ -13,11 +13,22 @@ module.exports = {
         return res.status(400).json({ message: 'Email already exists' });
       }
 
+      // test commit
+
       await User.create({
         firstName, lastName, username, email, password,
       });
 
       return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      return res.status(200).json({ message: 'User logged in successfully' });
     } catch (error) {
       next(error);
     }
